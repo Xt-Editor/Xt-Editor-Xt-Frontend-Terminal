6e55bf38994345276a9257c9e89165bf5736fcf2
Dom Rodriguez <shymega@shymega.org.uk>
2016-12-03 13:22:44 +0000

    Move logging/logger.rs -> logging/mod.rs

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -13,4 +13,24 @@
 // You should have received a copy of the GNU General Public License
 // along with Xtensis.  If not, see <http://www.gnu.org/licenses/>
 
-pub mod logger;
+extern crate fern;
+extern crate log;
+extern crate time;
+
+/// Initialises the global logger with the Fern backend.
+pub fn init_logger() {
+    let logging_config = fern::DispatchConfig {
+        format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
+            format!("[{}][{}] {}",
+                    time::now().strftime("%Y-%m-%d][%H:%M:%S").unwrap(),
+                    level,
+                    msg)
+        }),
+        output: vec![fern::OutputConfig::stdout(), fern::OutputConfig::file("output.log")],
+        level: log::LogLevelFilter::Trace,
+    };
+
+    if let Err(e) = fern::init_global_logger(logging_config, log::LogLevelFilter::Trace) {
+        panic!("Failed to init global logger: {}", e)
+    }
+}
